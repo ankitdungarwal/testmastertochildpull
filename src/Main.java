b139afbc84cbd5b6c278f42856dc634b078b801b
ankit dungarwal <ankitdungarwal92@gmail.com>
2022-06-24 10:54:30 +0530

    dev commit 1

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("from master branch initial print line statement");
     }
+
+    public void devMethod(){
+        System.out.println("method added in dev branch");
+    }
 }
