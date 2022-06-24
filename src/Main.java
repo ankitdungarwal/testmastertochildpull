4e8a14d9606e6d29433af2f69e6db3f0a86ca6c3
ankit dungarwal <ankitdungarwal92@gmail.com>
2022-06-24 10:56:22 +0530

    this is the first commit post initial commit

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("from master branch initial print line statement");
     }
+
+    public void mainBranchMethod(){
+        System.out.println("this is going to be the second thing I write in Main branch after the commit in dev branch");
+    }
 }
